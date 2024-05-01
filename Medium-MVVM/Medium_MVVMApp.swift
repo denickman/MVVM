bb9ecb9f94f8a2d85f48a8fae1438fd0f0c6ccce
Denis <denis.yaremenko@huma.com>
2024-05-01 08:54:41 +0300

    Initial Commit

---
+++ Medium-MVVM/Medium_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  Medium_MVVMApp.swift
+//  Medium-MVVM
+//
+//  Created by Denis Yaremenko on 01.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct Medium_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
